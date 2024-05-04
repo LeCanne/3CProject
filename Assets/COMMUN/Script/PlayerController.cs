18c5dd19c199eab1d257d86577f62766253ed610
Melvin Le Canne <94301906+LeCanne@users.noreply.github.com>
2024-05-04 16:31:56 +0200

    WalkAnim Labyrinth

--- Assets/COMMUN/Script/PlayerController.cs
+++ Assets/COMMUN/Script/PlayerController.cs
@@ -264,14 +264,15 @@ namespace _3CFeel.Controller
                     Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
                     skin.transform.rotation = Quaternion.Slerp(skin.transform.rotation, toRotation, Time.fixedDeltaTime / 0.06f);
                     direction.y = 0;
-                   
+                    anim.SetBool("isWalk", false);
+
                 }
                 else
                 {
                    
                     
                         skin.transform.localEulerAngles = new Vector3(0, camControl.gameObject.transform.eulerAngles.y, 0);
-                    
+                    anim.SetBool("isWalk", true);
                     
                 }
                
